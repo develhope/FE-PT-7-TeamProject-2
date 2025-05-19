628d3ee972d7f72b72f1108c588be64c41ad9d1a
Romina-Piazza <piazzaromina15m@gmail.com>
2025-05-19 19:16:03 +0200

    Aggiunte

---
+++ src/components/Casino/CasinoButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./CasinoButton.css";
+
+const CasinoButton = () => {
+  return (
+    <div className="wcc-central-button-wrapper">
+      <a href="/lotus-casino-details" className="button-link">Find out more</a>
+    </div>
+  );
+};
+
+export default CasinoButton;
