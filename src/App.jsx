7a7c61675cfb40f308375691dd75f8b5eef31cad
mariasorina2 <mariasorina2001@gmail.com>
2025-05-19 19:38:34 +0200

    added route of footer subpages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import { Routes, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage/Homepage'
 import PremiumExperiences from './pages/PremiunExperiences/PremiumExperiences'
 import FaqPage from './pages/FooterLinks/Faq/Faq'
+import SpaBooking from './pages/SpaBooking/SpaBooking'
 // import NoleggioAuto from './pages/NoleggioAuto/NoleggioAuto'
+import ContactUs from './pages/FooterLinks/ContactUs/ContactUs'
+import CancelReservation from './pages/FooterLinks/CancelReservation/CancelReservation'
 
 
 function App() {
@@ -12,6 +15,10 @@ function App() {
       <Route path="/premium-experiences" element={<PremiumExperiences />} />
       {/* <Route path="/noleggio" element={<NoleggioAuto />} /> */}
       <Route path="/faq-footer" element={<FaqPage />} />
+      <Route path="/spa-booking" element={<SpaBooking />} />
+      <Route path='/contact-us' element={<ContactUs />} />
+      <Route path='/cancel-reservation' element={<CancelReservation />} />
+
     </Routes>
   )
 }
