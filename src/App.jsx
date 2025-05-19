6feb5a25104d50f6b8e9a550b5c71d0802d5a626
mariasorina2 <mariasorina2001@gmail.com>
2025-05-19 20:57:11 +0200

    added nav and footer in the route

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,25 @@ import SpaBooking from './pages/SpaBooking/SpaBooking'
 // import NoleggioAuto from './pages/NoleggioAuto/NoleggioAuto'
 import ContactUs from './pages/FooterLinks/ContactUs/ContactUs'
 import CancelReservation from './pages/FooterLinks/CancelReservation/CancelReservation'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
 
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/premium-experiences" element={<PremiumExperiences />} />
-      {/* <Route path="/noleggio" element={<NoleggioAuto />} /> */}
-      <Route path="/faq-footer" element={<FaqPage />} />
-      <Route path="/spa-booking" element={<SpaBooking />} />
-      <Route path='/contact-us' element={<ContactUs />} />
-      <Route path='/cancel-reservation' element={<CancelReservation />} />
-
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/premium-experiences" element={<PremiumExperiences />} />
+        {/* <Route path="/noleggio" element={<NoleggioAuto />} /> */}
+        <Route path="/faq-footer" element={<FaqPage />} />
+        <Route path="/spa-booking" element={<SpaBooking />} />
+        <Route path='/contact-us' element={<ContactUs />} />
+        <Route path='/cancel-reservation' element={<CancelReservation />} />
+      </Routes>
+      <Footer />
+    </>
   )
 }
 
