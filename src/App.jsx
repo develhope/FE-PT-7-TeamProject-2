fee25de9066acd853d5b6f1333e4ff671b9d89fc
Romina-Piazza <piazzaromina15m@gmail.com>
2025-05-20 11:39:09 +0200

    aggiunta dettagli casino

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage/Homepage'
 import PremiumExperiences from './pages/PremiunExperiences/PremiumExperiences'
 import SpaBooking from './pages/SpaBooking/SpaBooking'
+import CasinoDetailsMain from './pages/LotusCasinoDetails/CasinoDetailsMain'
 // import NoleggioAuto from './pages/NoleggioAuto/NoleggioAuto'
 // import FaqPage from './pages/FooterLinks/Faq/Faq'
 
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<Homepage />} />
       <Route path="/spa-booking" element={<SpaBooking />} />
       <Route path="/premium-experiences" element={<PremiumExperiences />} />
+      <Route path="/CasinoDeatilsMain" element={<CasinoDetailsMain />} />
       {/* <Route path="/noleggio" element={<NoleggioAuto />} /> */}
       {/* <Route path="/faq-footer" element={<FaqPage />} /> */}
     </Routes>
