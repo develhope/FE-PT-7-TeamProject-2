2077857cc904028f183d90ea5fa1da053cd4d775
ffespo <ffesposti10@gmail.com>
2025-05-21 22:48:12 +0200

    feat: aggiunto componente ThingsToDoCard.jsx

---
+++ src/pages/ThingsToDo/ThingsToDoCard.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "./ThingsToDo.css";
+
+const CardThingsToDo = ({ id, title, description, details, price, images, reverse }) => {
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
+
+  return (
+    <div className={`cardThingsToDo card-${id} ${reverse ? "reverse" : ""}`}>
+      <div className="image-container">
+        <img src={images[activeImageIndex]} alt={title} className="card-img" />
+        <div className="dots">
+          {images.map((_, index) => (
+            <span
+              key={index}
+              className={`dot ${index === activeImageIndex ? "active" : ""}`}
+              onClick={() => setActiveImageIndex(index)}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="content">
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <ul>
+          {details.map((item, idx) => (
+            <li key={idx}>{item}</li>
+          ))}
+        </ul>
+        <div
+          className={`price-book ${
+            ["grand-canyon", "atv", "mojave"].includes(id) ? "change-color" : ""
+          }`}
+        >
+          <p className="price">{price}</p>
+          <button>Book</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardThingsToDo;
